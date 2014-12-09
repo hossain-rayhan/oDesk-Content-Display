02d35c335641241afb82b231ee8681d29f205f86
iftekhar-ahmed <bit0220iftekhar@gmail.com>
2014-12-09 12:19:42 +0600

    Add action bar progress loader.

--- app/src/main/java/sg/travelsmartrewards/contentdisplay/MainActivity.java
+++ app/src/main/java/sg/travelsmartrewards/contentdisplay/MainActivity.java
@@ -1,14 +1,12 @@
 package sg.travelsmartrewards.contentdisplay;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.ProgressDialog;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,12 +14,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 
 public class MainActivity extends Activity
@@ -32,6 +27,8 @@ public class MainActivity extends Activity
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
     private static WebView webView;
+    private View loadProgressView;
+    private MenuItem menuItemLoadProgress;
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
@@ -61,6 +58,14 @@ public class MainActivity extends Activity
         fragmentManager.beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position))
                 .commit();
+    }
+
+    private void showLoadProgress(boolean show) {
+        if (show) {
+            menuItemLoadProgress.setVisible(true);
+        } else {
+            menuItemLoadProgress.setVisible(false);
+        }
     }
 
     private void showProgress(final boolean show) {
@@ -106,16 +111,13 @@ public class MainActivity extends Activity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       /* if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);*/
-        return false;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menuItemLoadProgress = menu.findItem(R.id.action_load_progress);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        loadProgressView = inflater.inflate(R.layout.refresh_progress, null);
+        MenuItemCompat.setActionView(menuItemLoadProgress, loadProgressView);
+        menuItemLoadProgress.setVisible(false);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -203,7 +205,7 @@ public class MainActivity extends Activity
             webView = (WebView) rootView.findViewById(R.id.webview);
             webView.setWebViewClient(new WebViewClient() {
 
-                ProgressDialog progressDialog;
+                // ProgressDialog progressDialog;
 
                 //If you will not use this method url links are opeen in new brower not in webview
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -213,23 +215,25 @@ public class MainActivity extends Activity
 
                 //Show loader on url load
                 public void onLoadResource(WebView view, String url) {
-                    if (progressDialog == null) {
+                    /*if (progressDialog == null) {
                         // in standard case YourActivity.this
                         progressDialog = new ProgressDialog(getActivity());
                         progressDialog.setMessage("Loading...");
                         progressDialog.show();
-                    }
+                    }*/
+                    ((MainActivity) getActivity()).showLoadProgress(true);
                 }
 
                 public void onPageFinished(WebView view, String url) {
-                    try {
+                    /*try {
                         if (progressDialog.isShowing()) {
                             progressDialog.dismiss();
                             progressDialog = null;
                         }
                     } catch (Exception exception) {
                         exception.printStackTrace();
-                    }
+                    }*/
+                    ((MainActivity) getActivity()).showLoadProgress(false);
                 }
             });
             WebSettings webSettings = webView.getSettings();
--- app/src/main/java/sg/travelsmartrewards/contentdisplay/NavigationDrawerFragment.java
+++ app/src/main/java/sg/travelsmartrewards/contentdisplay/NavigationDrawerFragment.java
@@ -1,16 +1,16 @@
 package sg.travelsmartrewards.contentdisplay;
 
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,7 +20,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -100,7 +99,7 @@ public class NavigationDrawerFragment extends Fragment {
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
                 android.R.layout.simple_list_item_activated_1,
-                android.R.id.text1,getResources().getStringArray(R.array.drawer_menu)));
+                android.R.id.text1, getResources().getStringArray(R.array.drawer_menu)));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
@@ -239,11 +238,6 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
